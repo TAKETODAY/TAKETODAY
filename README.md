09da04622efa0b79a9d671045eb328d23e018202
TODAY <2228592280@qq.com>
2020-09-17 10:45:45 +0800

    :memo: 更新README

---
+++ README.md
@@ -0,0 +1,26 @@
+<table align="center"><tr>
+<td valign="top" width="50%">
+
+### 我的博客
+- [大学写的一个 Java Web 库](https://taketoday.cn/articles/1553922073210)
+- [STM32 点亮LED](https://taketoday.cn/articles/1520946546731)
+- [Visual Studio 2019 添加dll库](https://taketoday.cn/articles/1565229446508)
+- [STM32 按键](https://taketoday.cn/articles/1521552446255)
+- [Visual Studio 2019 环境下如何解决Qt5中文乱码问题？](https://taketoday.cn/articles/1567093123126)
+- [STM32 IWDG独立看门狗](https://taketoday.cn/articles/1525268062768)
+- [STM32 USART串口通讯](https://taketoday.cn/articles/1525261875788)
+- [STM32 外部中断](https://taketoday.cn/articles/1526218899315)
+
+[查看更多](https://taketoday.cn)
+
+</td>
+<td valign="top" width="50%">
+
+### GitHub统计
+<p align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=TAKETODAY"/>
+</p>
+
+> 2020-09-17 10:45:11
+</td>
+</tr></table>
---
+++ generate.py
@@ -0,0 +1,60 @@
+﻿# -*- coding: utf-8 -*-
+import json
+import urllib3
+from datetime import datetime
+
+articlesUrl = 'https://taketoday.cn/api/articles/popular'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) GitHubREADME/85.0.4183.102 Safari/537.36'
+}
+
+
+def addIntro(file):
+    ret = ''''''
+    file.write(ret)
+
+
+def addProjectStats(file):
+    text = '''
+### GitHub统计
+<p align="center">
+  <img src="https://github-readme-stats.vercel.app/api?username=TAKETODAY"/>
+</p>
+
+> '''
+    text += datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    file.write(text)
+
+
+def addBlog(file):
+    http = urllib3.PoolManager(num_pools=1, headers=headers)
+    response = http.request('GET', articlesUrl)
+    file.write("\n### 我的博客\n")
+
+    articles = json.loads(response.data)
+
+    for article in articles:
+        file.write(
+            '- [%s](https://taketoday.cn/articles/%s)\n' % (article['title'], article['id'])
+        )
+
+    file.write('\n[查看更多](https://taketoday.cn)\n')
+
+
+if __name__ == '__main__':
+    file = open('README.md', 'w+', encoding="utf-8")
+    addIntro(file)
+    file.write('<table align="center"><tr>\n')
+    file.write('<td valign="top" width="50%">\n')
+
+    addBlog(file)
+
+    file.write('\n</td>\n')
+    file.write('<td valign="top" width="50%">\n')
+
+    addProjectStats(file)
+
+    file.write('\n</td>\n')
+    file.write('</tr></table>\n')
+    file.close()
